2c7cb5e5897c2ac0d7a1a370d9319502f7a4d43b
jlooper <jen.looper@gmail.com>
2018-02-28 14:10:31 -0500

    finishing touches!

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -4,11 +4,11 @@
         <NavigationButton text="Back!" android.systemIcon="ic_menu_back" @tap="$router.back()" />
       </ActionBar>
       <StackLayout>
-        <Label :text="message" textWrap="true"/>
-        <GridLayout columns="auto,auto" rows="auto,auto">
-          <Button row="0" col="0" @tap="decrement" text="-"/>
-          <Button row="0" col="1" @tap="increment" text="+"/>
-          <Button row="1" colSpan="2" text="Psssh it's boring, take me home" @tap="$router.go(-1)" />
+        <Label horizontalAlignment="center" class="large" :text="message" textWrap="true"/>
+        <GridLayout columns="*,*" rows="auto,auto">
+          <Button class="counter" row="0" col="0" @tap="decrement" text="-"/>
+          <Button class="counter" row="0" col="1" @tap="increment" text="+"/>
+          <Button row="1" colSpan="2" text="Take me home" @tap="$router.go(-1)" />
         </GridLayout>
       </StackLayout>
     </Page>
@@ -20,7 +20,7 @@
   export default {
     computed: {
       message () {
-        return `Counter: ${this.$store.state.counter.count}`
+        return `I love you this much! ${this.$store.state.counter.count}`
       }
     },
     methods: mapActions([
