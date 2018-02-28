7b39595698bf5bc91efecb99eb487c7b41805eae
jlooper <jen.looper@gmail.com>
2018-02-28 13:46:55 -0500

    vue files

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -4,7 +4,28 @@
         <NavigationButton text="Back!" android.systemIcon="ic_menu_back" @tap="$router.back()" />
       </ActionBar>
       <StackLayout>
-        <Button text="Psssh it's boring, take me home" @tap="$router.go(-1)" />
+        <Label :text="message" textWrap="true"/>
+        <GridLayout columns="auto,auto" rows="auto,auto">
+          <Button row="0" col="0" @tap="decrement" text="-"/>
+          <Button row="0" col="1" @tap="increment" text="+"/>
+          <Button row="1" colSpan="2" text="Psssh it's boring, take me home" @tap="$router.go(-1)" />
+        </GridLayout>
       </StackLayout>
     </Page>
 </template>
+
+<script>
+  import { mapActions } from 'vuex';
+
+  export default {
+    computed: {
+      message () {
+        return `Counter: ${this.$store.state.counter.count}`
+      }
+    },
+    methods: mapActions([
+      'decrement',
+      'increment',
+    ]),
+  };
+</script>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 const Vue = require('nativescript-vue');
 import router from './router/routes';
+import store from './store';
 import './styles.scss';
+
 router.replace('/home')
 new Vue({
-  router
+  router,
+  store
 }).$start();
